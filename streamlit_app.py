b63a00345a73e5b76fdee7ec92fdc4143d470e96
randycaleb595 <randycaleb595@gmail.com>
2026-07-11 01:43:45 -0700

    Remove redundant lines in output HTML generation

--- streamlit_app.py
+++ streamlit_app.py
@@ -268,11 +268,3 @@ if st.button(text['btn']):
                             part_of_speech = meaning.get('partOfSpeech', 'noun')
                             definition = meaning['definitions'][0]['definition']
                             output_html += f"<li><b>[{part_of_speech}]</b> {definition}</li>"
-
-
-
-# Custom CSS styling to make the background strictly black and fonts completely sans-serif 
-
-
-                  
-
