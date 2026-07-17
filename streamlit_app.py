dc17a408ee16965bd49b7a39db39645172d5d0eb
randycaleb595 <randycaleb595@gmail.com>
2026-07-17 18:01:09 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
+from google import genai
 import requests
 import streamlit as st
-from google import genai
 # ─── CONFIG ───────────────────────────────────────────────────────────────────
 GEMINI_API_KEY = ""
 LANG_LABELS = {
